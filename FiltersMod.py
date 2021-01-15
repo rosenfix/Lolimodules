1bb6351397a317995248f0ffb2ecc3d09bd8855e
rosenfix <55348280+rosenfix@users.noreply.github.com>
2021-01-15 16:55:47 +0200

    Update FiltersMod.py

--- FiltersMod.py
+++ FiltersMod.py
@@ -16,7 +16,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#  SOURCE: rf0x3d.su
 
 from .. import loader, utils
 import functools
